c62f86fbc938b50ab6cb75a5e5e04c61cc539098
Joseph <jtibbs@uni.edu>
2020-02-02 21:33:20 -0600

    Streamlined Editing
    (not yet to automated processing)

--- AFMAnalyze.m
+++ AFMAnalyze.m
@@ -1,8 +1,7 @@
 %Wrapper function to hold analysis code for tracing program
 %To begin, select folder which contains .txt files
 
-dirName = uigetdir;
-dir1 = dir([dirName filesep '*.txt']);
+
 imageSize = [600 600];
 imageDim = [256 256];
 pixel = imageSize(1)/imageDim(1);
@@ -12,8 +11,13 @@ if ~exist('j','var')
     j = 1;
 end
 
+if ~exist('dirName','var')
+    dirName = uigetdir;
+    dir1 = dir([dirName filesep '*.txt']);
+end
+
 if ~exist('analysis','var')
-    analysis = struct([1 0]);
+    analysis = struct([]);
 end
 
 save([dirName filesep 'zBackup ' int2str(sum(fix(clock)))],'analysis','j');
@@ -33,7 +37,7 @@ while j <= length(dir1)
     [flat1,image1] = quadFlattenN(image1);
     figure(); imagesc(flat1);
     %image1 = image1./max(image1(:));
-    Answer = questdlg('Any things here you want to capture?');
+    Answer = questdlg('Any things here you want to capture?','Analyze image?','Yes','No','Debug','Yes');
     if strcmp(Answer,'Yes')
         currentAnalysis = length(analysis);
         analysis(currentAnalysis+1).Xframe = 0;
@@ -45,7 +49,7 @@ while j <= length(dir1)
         [matrix1,image2] = quadFlattenN(flat1(Yframe(1):Yframe(2),Xframe(1):Xframe(2)));
         [surfX, surfY] = meshgrid(1:size(matrix1,2),1:size(matrix1,1));
         figure(4); surf(surfX,surfY,matrix1,'EdgeColor','none','FaceColor','interp'); hold on;
-        figure(3); imagesc(matrix1); hold on;
+        figure(k); imagesc(matrix1); hold on;
         [X0,Y0] = ginput(4);
         analysis(end).X0 = X0;
         analysis(end).Y0 = Y0;
@@ -62,18 +66,18 @@ while j <= length(dir1)
     %     [localMeshX, localMeshY] = meshgrid(localCoordsX, localCoordsY);
     %     localMatrix = matrix1(localCoordsY, localCoordsX);
         F = griddedInterpolant(matrix1);
-        figure(); plot(linspace(X0(1),X0(2),20), F(linspace(Y0(1),Y0(2),20), linspace(X0(1),X0(2),20)));
+        figure(2); plot(linspace(X0(1),X0(2),20), F(linspace(Y0(1),Y0(2),20), linspace(X0(1),X0(2),20)));
         clear trace h
         [trace(1,1),~] = ginput(1);
         trace(1,2) = (Y0(1)-Y0(2))/(X0(1)-X0(2))*(trace(1)-X0(1))+Y0(1);
         analysis(end).initPoint = trace(1,:);
         h(1) = F(trace(1,2),trace(1,1));
-        [trace(2,:),h(2)] = followChain([X0(1) Y0(1)], [trace(1,1) trace(1,2)], F, 5/pixels);
+        [trace(2,:),h(2)] = followChain([X0(1) Y0(1)], [trace(1,1) trace(1,2)], F, 5/pixel);
         i = 3;
         %sideChain = (trace(2,2)>(Y0(3)+(Y0(4)-Y0(3))/(X0(4)-X0(3))*(trace(2,1)-X0(3))));
         onChain = 1;
         while onChain
-            [trace(i,:),h(i)] = followChain(trace(i-2,:), trace(i-1,:), F, 2);
+            [trace(i,:),h(i)] = followChain(trace(i-2,:), trace(i-1,:), F, 5/pixel);
             if i>20
                 %onChain = (sideChain==(trace(i,2)>(Y0(3)+(Y0(4)-Y0(3))/(X0(4)-X0(3))*(trace(i,1)-X0(3)))));
                 %sideChain = (trace(i,2)>(Y0(3)+(Y0(4)-Y0(3))/(X0(4)-X0(3))*(trace(i,1)-X0(3))));
@@ -85,32 +89,38 @@ while j <= length(dir1)
             if mod(i,1000)==0
                 keyboard;
             end
-            F = griddedInterpolant(matrix1);
+            %F = griddedInterpolant(matrix1);
         end
-        figure(3); plot(trace(:,1),trace(:,2));
         figure(4); plot3(trace(:,1),trace(:,2),h);
+        figure(k); plot(trace(:,1),trace(:,2));
         analysis(end).trace = trace;
         analysis(end).h = h;
         analysis(end).path = [dir1(j).folder filesep dir1(j).name];
         analysis(end).fileName = dir1(j).name;
+        anS = questdlg('Do you approve of the fit?',...
+        'Choose wisely', 'Yes', 'No','Yes');
+        if strcmp(anS,'No')
+            analysis(end) = [];
+            close(k);
+            k = k-1;
+        end
+        k = k+1;
     elseif strcmp(Answer,'No')
         j = j+1;
 %         traces{length(traces)+1} = trace;
 %         hs{length(hs)+1} = h;
 %         dirList{length(dirList)+1} = [dir1(j).folder filesep dir1(j).name];
-        close all;
+        %close all;
     else
-        j = 100000;
+        keyboard;
     end
-    anS = questdlg('Do you approve of the fit? If yes, click yes and then "continue"',...
-        'Choose wisely', 'Yes', 'No');
-    if strcmp(anS,'No')
-        analysis(end) = [];
-    end
-    keyboard;
+    
     try
-        close 3;
         close 4;
+        close 2;
+        close 3;
+
     catch
     end
 end
+save([dirName filesep 'zBackup ' int2str(sum(fix(clock)))],'analysis','j');
--- followChain.m
+++ followChain.m
@@ -1,36 +1,39 @@
-function [newPoint,h] = followChain(point1, point2, ~, pixels)
-    vec1 = (point2-point1)/norm(point2-point1);
-    point2 = reshape(point2,[2 1]);
-    vec1 = reshape(vec1,[2 1]);
-    i = 1;
-    radList = -pi/3:.015:pi/3;
-    section = zeros([1 length(radList)]);
-    for rad = radList
-        additionVec = [cos(rad) -sin(rad); sin(rad) cos(rad)]*vec1*pixels;
-        section(i) = interpM(point2(2)+additionVec(2),point2(1)+additionVec(1));
-        i = i+1;
-    end
-    radFinal = ((section.^2).*radList/(sum(section.^2)));
-    newPoint = point2 + [cos(radFinal) -sin(radFinal); sin(radFinal) cos(radFinal)]*vec1*pixels;
-    newPoint = newPoint';
-    h = interpM(newPoint(2),newPoint(1));
+function [newPoint,h] = followChain(point1, point2, interpM, pixels)
+%     vec1 = (point2-point1)/norm(point2-point1);
+%     point2 = reshape(point2,[2 1]);
+%     vec1 = reshape(vec1,[2 1]);
+%     i = 1;
+%     radList = -pi/3:.015:pi/3;
+%     section = zeros([1 length(radList)]);
+%     for rad = radList
+%         additionVec = [cos(rad) -sin(rad); sin(rad) cos(rad)]*vec1*pixels;
+%         section(i) = interpM(point2(2)+additionVec(2),point2(1)+additionVec(1));
+%         i = i+1;
+%     end
+%     radFinal = sum((section).*radList/(sum(section)));
+%     newPoint = point2 + [cos(radFinal) -sin(radFinal); sin(radFinal) cos(radFinal)]*vec1*pixels;
+%     newPoint = newPoint';
+%     h = interpM(newPoint(2),newPoint(1));
 
 
 % Uncomment below to use linear section method
-%     vec1 = (point2-point1)/norm(point2-point1);
-%     testPoint = [point2(1)+pixels*vec1(1), point2(2)+pixels*vec1(2)];
-%     for i = 1:3
-%         curveParameter = 2;
-%         sectionX = linspace(testPoint(1)-curveParameter*pixels*vec1(2), testPoint(1)+curveParameter*pixels*vec1(2), 100);
-%         sectionY = linspace(testPoint(2)+curveParameter*pixels*vec1(1), testPoint(2)-curveParameter*pixels*vec1(1), 100);
-%         section = interpM(sectionY,sectionX);
-%         weightX = sum(sectionX.*section)/sum(section);
-%         weightY = sum(sectionY.*section)/sum(section);
-% %         [~,I] = max(section);
-%         testPoint = [weightX,weightY];
-%     end
-% newPoint = testPoint;
-% h = interpM(testPoint(2),testPoint(1));
+    vec1 = (point2-point1)/norm(point2-point1);
+    testPoint = [point2(1)+pixels*vec1(1), point2(2)+pixels*vec1(2)];
+    for i = 1:3
+        curveParameter = 2;
+        sectionX = linspace(testPoint(1)-curveParameter*pixels*vec1(2), testPoint(1)+curveParameter*pixels*vec1(2), 100);
+        sectionY = linspace(testPoint(2)+curveParameter*pixels*vec1(1), testPoint(2)-curveParameter*pixels*vec1(1), 100);
+        section = interpM(sectionY,sectionX);
+        section = section-min(section);
+        weightX = sum(sectionX.*section)/sum(section);
+        weightY = sum(sectionY.*section)/sum(section);
+%         [~,I] = max(section);
+        testPoint = [weightX,weightY];
+    end
+vec2 = testPoint-point2;
+vec2 = pixels*vec2./norm(vec2);
+newPoint = point2+vec2;
+h = interpM(newPoint(2),newPoint(1));
 
 end
 
